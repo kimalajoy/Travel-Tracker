95cc53c136e19462e40caf0c45403870ab17c625
Kimala Cochran <kimalajoy@gmail.com>
2020-02-27 18:29:12 -0700

    started styling

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import $ from 'jquery';
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import './css/variables.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import './images/turing-logo.png';
+import './images/corey-agopian-5y4ljzRrDFA-unsplash.jpg';
 
 console.log('This is the JavaScript entry file - your code begins here.');
